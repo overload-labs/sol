329ca8432eb8db556a6b5eb5f8fe3aa4ca9237a1
egozoq <egozoq@proton.me>
2024-06-07 21:37:47 +0200

    test: consensus revert and no hook contracts

--- test/Overload.consensus.t.sol
+++ test/Overload.consensus.t.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.8.0;
 
 import {Test, console, console2, stdError} from "forge-std/Test.sol";
 
-import {DelegationKey} from "../src/libraries/types/Delegation.sol";
+import {DelegationNotFound, DelegationKey} from "../src/libraries/types/Delegation.sol";
+import {UndelegationNotFound, UndelegationKey} from "../src/libraries/types/Undelegation.sol";
 import {TokenIdLib} from "../src/libraries/TokenIdLib.sol";
 import {Overload} from "../src/Overload.sol";
 
-import {ConsensusRevertAllMock} from "./mocks/ConsensusMock.sol";
+import {
+    ConsensusRevertDelegateMock,
+    ConsensusRevertRedelegateMock,
+    ConsensusRevertUndelegatingMock,
+    ConsensusRevertUndelegateMock,
+    ConsensusNoHook
+} from "./mocks/ConsensusMock.sol";
 import {ERC20Fee} from "./mocks/ERC20Fee.sol";
 import {ERC20Mock} from "./mocks/ERC20Mock.sol";
 
@@ -20,33 +27,126 @@ contract OverloadConsensusTest is Test {
 
     Overload public overload;
     ERC20Mock public token;
-    ConsensusRevertAllMock public consensusRevert;
+    ConsensusRevertDelegateMock public consensusRevert;
+    ConsensusRevertRedelegateMock public consensusRevertRedelegateMock;
+    ConsensusRevertUndelegatingMock public consensusRevertUndelegating;
+    ConsensusRevertUndelegateMock public consensusRevertUndelegateMock;
+    ConsensusNoHook public consensusNoHook;
 
     function setUp() public {
         overload = new Overload();
         token = new ERC20Mock("Test", "TEST", 18);
-        consensusRevert = new ConsensusRevertAllMock(address(overload));
+        consensusRevert = new ConsensusRevertDelegateMock(address(overload));
+        consensusRevertRedelegateMock = new ConsensusRevertRedelegateMock(address(overload));
+        consensusRevertUndelegating = new ConsensusRevertUndelegatingMock(address(overload));
+        consensusRevertUndelegateMock = new ConsensusRevertUndelegateMock(address(overload));
+        consensusNoHook = new ConsensusNoHook();
     }
 
     /*//////////////////////////////////////////////////////////////
                                 HELPERS
     //////////////////////////////////////////////////////////////*/
 
-    function deposit(address user) public {
-        token.mint(user, 100);
+    function deposit(address user, uint256 amount) public {
+        token.mint(user, amount);
 
         vm.prank(user);
-        token.approve(address(overload), 100);
+        token.approve(address(overload), amount);
         vm.prank(user);
-        overload.deposit(user, address(token), 100);
+        overload.deposit(user, address(token), amount);
+    }
+
+    function deposit(ERC20Mock token_, address user, uint256 amount) public {
+        token_.mint(user, amount);
+
+        vm.prank(user);
+        token_.approve(address(overload), amount);
+        vm.prank(user);
+        overload.deposit(user, address(token_), amount);
+    }
+
+    function delegate(address user, address consensus, address validator, uint256 amount, bool strict) public {
+        DelegationKey memory key = DelegationKey({
+            owner: user,
+            token: address(token),
+            consensus: consensus,
+            validator: validator
+        });
+
+        vm.prank(user);
+        overload.delegate(key, amount, "", strict);
+    }
+
+    function delegate(ERC20Mock token_, address user, address consensus, address validator, uint256 amount, bool strict) public {
+        DelegationKey memory key = DelegationKey({
+            owner: user,
+            token: address(token_),
+            consensus: consensus,
+            validator: validator
+        });
+
+        vm.prank(user);
+        overload.delegate(key, amount, "", strict);
+    }
+
+    function redelegate(address user, address consensus, address fromValidator, address toValidator, bool strict) public returns (bool) {
+        DelegationKey memory fromKey = DelegationKey({
+            owner: user,
+            token: address(token),
+            consensus: consensus,
+            validator: fromValidator
+        });
+        DelegationKey memory toKey = DelegationKey({
+            owner: user,
+            token: address(token),
+            consensus: consensus,
+            validator: toValidator
+        });
+
+        vm.prank(user);
+        return overload.redelegate(fromKey, toKey, "", strict);
+    }
+
+    function undelegating(address user, address consensus, address validator, uint256 amount, bool strict) public returns (bool success, UndelegationKey memory, uint256) {
+        DelegationKey memory key = DelegationKey({
+            owner: user,
+            token: address(token),
+            consensus: consensus,
+            validator: validator
+        });
+
+        vm.prank(user);
+        return overload.undelegating(key, amount, "", strict);
+    }
+
+    function undelegating(ERC20Mock token_, address user, address consensus, address validator, uint256 amount, bool strict) public returns (bool success, UndelegationKey memory, uint256) {
+        DelegationKey memory key = DelegationKey({
+            owner: user,
+            token: address(token_),
+            consensus: consensus,
+            validator: validator
+        });
+
+        vm.prank(user);
+        return overload.undelegating(key, amount, "", strict);
+    }
+
+    function undelegate(address user, UndelegationKey memory ukey, int256 position, bytes memory data, bool strict) public {
+        vm.prank(user);
+        overload.undelegate(ukey, position, data, strict);
+    }
+
+    function setUndelegatingDelay(address consensus, uint256 delay) public {
+        vm.prank(consensus);
+        overload.setUndelegatingDelay(consensus, delay);
     }
 
     /*//////////////////////////////////////////////////////////////
                             CONSENSUS REVERT
     //////////////////////////////////////////////////////////////*/
 
-    function test_delegate() public {
-        deposit(address(0xBEEF));
+    function test_fail_delegate_consensusRevert() public {
+        deposit(address(0xBEEF), 100);
 
         DelegationKey memory key = DelegationKey({
             owner: address(0xBEEF),
@@ -55,16 +155,117 @@ contract OverloadConsensusTest is Test {
             validator: address(0xCCCC)
         });
 
+        // Strict true reverts
         vm.prank(address(0xBEEF));
         vm.expectRevert();
         overload.delegate(key, 50, "", true);
         assertEq(overload.getDelegationsLength(address(0xBEEF), address(token)), 0);
 
+        // Strict false passes
         vm.prank(address(0xBEEF));
         overload.delegate(key, 50, "", false);
         assertEq(overload.getDelegationsLength(address(0xBEEF), address(token)), 1);
         assertEq(overload.getDelegation(address(0xBEEF), address(token), 0).consensus, address(consensusRevert));
         assertEq(overload.getDelegation(address(0xBEEF), address(token), 0).validator, address(0xCCCC));
         assertEq(overload.getDelegation(address(0xBEEF), address(token), 0).amount, 50);
+    }
+
+    function test_redelegate() public {
+        deposit(address(0xBEEF), 1000);
+        delegate(address(0xBEEF), address(consensusRevertRedelegateMock), address(0xFFFF), 50, true);
+
+        // Should revert
+        vm.expectRevert();
+        redelegate(address(0xBEEF), address(consensusRevertRedelegateMock), address(0xFFFF), address(0xEEEE), true);
+
+        // Should not revert
+        redelegate(address(0xBEEF), address(consensusRevertRedelegateMock), address(0xFFFF), address(0xEEEE), false);
+    }
+
+    function test_fail_undelegating_consensusRevert() public {
+        // Deposit and delegate
+        deposit(address(0xBEEF), 100);
+        delegate(address(0xBEEF), address(consensusRevertUndelegating), address(0xFFFF), 50, true);
+
+        // Undelegating
+        DelegationKey memory key = DelegationKey({
+            owner: address(0xBEEF),
+            token: address(token),
+            consensus: address(consensusRevertUndelegating),
+            validator: address(0xFFFF)
+        });
+
+        // Should revert
+        vm.prank(address(0xBEEF));
+        vm.expectRevert();
+        overload.undelegating(key, 25, "", true);
+
+        // hould not revert
+        vm.prank(address(0xBEEF));
+        overload.undelegating(key, 25, "", false);
+    }
+
+    function test_fail_undelegate_consensusRevert() public {
+        setUndelegatingDelay(address(consensusRevertUndelegateMock), 500);
+        deposit(address(0xBEEF), 100);
+        delegate(address(0xBEEF), address(consensusRevertUndelegateMock), address(0xFFFF), 100, true);
+
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 0);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 100);
+
+        (, UndelegationKey memory ukey, uint256 index) = undelegating(address(0xBEEF), address(consensusRevertUndelegateMock), address(0xFFFF), 50, true);
+
+        // The bonded is still the same here.
+        // `balanceOf` + `bonded` needs to always equal the principal token amount.
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 0);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 100);
+
+        // Should revert
+        vm.warp(501);
+        vm.expectRevert();
+        undelegate(address(0xBEEF), ukey, int256(index), "", true);
+
+        // Should not revert
+        undelegate(address(0xBEEF), ukey, int256(index), "", false);
+
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 50);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 50);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                NO HOOKS
+    //////////////////////////////////////////////////////////////*/
+
+    function test_fail_noHooks() public {
+        setUndelegatingDelay(address(consensusNoHook), 500);
+        deposit(address(0xBEEF), 100);
+
+        // Should revert
+        vm.expectRevert();
+        delegate(address(0xBEEF), address(consensusNoHook), address(0xFFFF), 100, true);
+        // Should not revert
+        delegate(address(0xBEEF), address(consensusNoHook), address(0xFFFF), 100, false);
+
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 0);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 100);
+
+        // Should revert
+        vm.expectRevert();
+        undelegating(address(0xBEEF), address(consensusNoHook), address(0xFFFF), 100, true);
+        // Should not revert
+        (, UndelegationKey memory ukey, uint256 index) = undelegating(address(0xBEEF), address(consensusNoHook), address(0xFFFF), 100, false);
+
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 0);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 100);
+
+        vm.warp(block.timestamp + 500);
+        // Should revert
+        vm.expectRevert();
+        undelegate(address(0xBEEF), ukey, int256(index), "", true);
+        // Should not revert
+        undelegate(address(0xBEEF), ukey, int256(index), "", false);
+
+        assertEq(overload.balanceOf(address(0xBEEF), address(token).convertToId()), 100);
+        assertEq(overload.bonded(address(0xBEEF), address(token)), 0);
     }
 }
--- test/mocks/ConsensusMock.sol
+++ test/mocks/ConsensusMock.sol
@@ -23,7 +23,7 @@ contract ConsensusBeforeDelegateMock {
     function test() public {}
 }
 
-contract ConsensusRevertAllMock is IHOverload {
+contract ConsensusRevertDelegateMock is IHOverload {
     // Core contract
     address public overload;
 
@@ -39,6 +39,40 @@ contract ConsensusRevertAllMock is IHOverload {
         revert();
     }
 
+    function beforeRedelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterRedelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeUndelegating(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegating(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == IHOverload.beforeDelegate.selector ||
+            interfaceId == IHOverload.afterDelegate.selector;
+    }
+
+    function test() public {}
+}
+
+contract ConsensusRevertRedelegateMock is IHOverload {
+    // Core contract
+    address public overload;
+
+    constructor(address overload_) {
+        overload = overload_;
+    }
+
+    function beforeDelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterDelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
     function beforeRedelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {
         revert();
     }
@@ -47,6 +81,40 @@ contract ConsensusRevertAllMock is IHOverload {
         revert();
     }
 
+    function beforeUndelegating(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegating(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == IHOverload.beforeRedelegate.selector ||
+            interfaceId == IHOverload.afterRedelegate.selector;
+    }
+
+    function test() public {}
+}
+
+contract ConsensusRevertUndelegatingMock is IHOverload {
+    // Core contract
+    address public overload;
+
+    constructor(address overload_) {
+        overload = overload_;
+    }
+
+    function beforeDelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterDelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeRedelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterRedelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
     function beforeUndelegating(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {
         revert();
     }
@@ -54,6 +122,40 @@ contract ConsensusRevertAllMock is IHOverload {
     function afterUndelegating(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {
         revert();
     }
+
+    function beforeUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {}
+
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == IHOverload.beforeUndelegating.selector ||
+            interfaceId == IHOverload.afterUndelegating.selector;
+    }
+
+    function test() public {}
+}
+
+contract ConsensusRevertUndelegateMock is IHOverload {
+    // Core contract
+    address public overload;
+
+    constructor(address overload_) {
+        overload = overload_;
+    }
+
+    function beforeDelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterDelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeRedelegate(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterRedelegate(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
+
+    function beforeUndelegating(address, DelegationKey memory, uint256, bytes calldata) external pure returns (bytes4) {}
+
+    function afterUndelegating(address, DelegationKey memory, uint256, Delegation memory, bytes calldata) external pure returns (bytes4) {}
 
     function beforeUndelegate(address, UndelegationKey memory, bytes calldata) external pure returns (bytes4) {
         revert();
@@ -66,6 +168,17 @@ contract ConsensusRevertAllMock is IHOverload {
     function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
         return
             interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
+            interfaceId == IHOverload.beforeUndelegate.selector ||
+            interfaceId == IHOverload.afterUndelegate.selector;
+    }
+
+    function test() public {}
+}
+
+contract ConsensusNoHook {
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return
+            interfaceId == 0x01ffc9a7 || // ERC165 Interface ID for ERC165
             interfaceId == IHOverload.beforeDelegate.selector ||
             interfaceId == IHOverload.afterDelegate.selector ||
             interfaceId == IHOverload.beforeRedelegate.selector ||
@@ -75,6 +188,4 @@ contract ConsensusRevertAllMock is IHOverload {
             interfaceId == IHOverload.beforeUndelegate.selector ||
             interfaceId == IHOverload.afterUndelegate.selector;
     }
-
-    function test() public {}
 }
